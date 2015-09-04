deeebc39fea254e2bf38ccb8a85b939f442204e6
David Taieb <david_taieb@us.ibm.com>
2015-09-04 16:41:48 -0400

    Update

--- helloSpark/src/main/scala/com/ibm/cds/spark/samples/helloSpark.scala
+++ helloSpark/src/main/scala/com/ibm/cds/spark/samples/helloSpark.scala
@@ -1,23 +1,24 @@
 package com.ibm.cds.spark.samples
 
 import org.apache.spark._
-import org.apache.spark.rdd.RDD.numericRDDToDoubleRDDFunctions
 
 object HelloSpark {
+  
+  //main method invoked when running as a standalone Spark Application
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Hello Spark")
     val spark = new SparkContext(conf)
 
-    println("Hello Spark Demo. Compute the mean and variance of a randomly generated collection")
-    println("Usage: computeStatsForRandomCollection( sparkContext, countPerPartitions, partitions")
-    val stats = computeStatsForRandomCollection(spark);
+    println("Hello Spark Demo. Compute the mean and variance of a collection")
+    val stats = computeStatsForCollection(spark);
     println(">>> Results: ")
     println(">>>>>>>Mean: " + stats._1 );
     println(">>>>>>>Variance: " + stats._2);
     spark.stop()
   }
   
-  def computeStatsForRandomCollection( spark: SparkContext, countPerPartitions: Int = 100000, partitions: Int=5): (Double, Double) = {    
+  //Library method that can be invoked from Jupyter Notebook
+  def computeStatsForCollection( spark: SparkContext, countPerPartitions: Int = 100000, partitions: Int=5): (Double, Double) = {    
     val totalNumber = math.min( countPerPartitions * partitions, Long.MaxValue).toInt;
     val rdd = spark.parallelize( 1 until totalNumber,partitions);
     (rdd.mean(), rdd.variance())
