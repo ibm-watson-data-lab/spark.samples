9343263c8e86bf9fe3f6d9755d1979e084e1ef1c
David Taieb <david_taieb@us.ibm.com>
2016-06-28 14:34:04 -0400

    added url

--- pixiedust/setup.py
+++ pixiedust/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='pixiedust',
       version='0.1',
       description='Misc helpers for Spark Python Notebook',
-      url='',
+      url='https://github.com/ibm-cds-labs/spark.samples/tree/master/pixiedust',
       install_requires=['maven-artifact'],
       author='David Taieb',
       author_email='david_taieb@us.ibm.com',
       license='Apache 2.0',
-      packages=['packageManager'],
+      packages=['packageManager','display'],
       zip_safe=False)
